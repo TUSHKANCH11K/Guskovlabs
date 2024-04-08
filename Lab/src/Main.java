23bb14b2d15b31c1052b79d68caf5ea7987bb5ba
Виктория Кудяева <vika_kudyaeva@mail.ru>
2024-04-08 15:46:08 +0400

    лаб 2 итог

--- Lab/src/Main.java
+++ Lab/src/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final int NUM_ELEMENTS = 100000;
+    private static int[] arr1 = new int[NUM_ELEMENTS]; // initial
+    private static int n = 100000;
+    private static int[] arr = new int[NUM_ELEMENTS]; // fillable
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,6 +17,33 @@ public class Main {
         int[] dimensions = {10, 30, 5, 60}; // Размеры матриц: A1(10x30), A2(30x5), A3(5x60)
         System.out.println("Минимальное количество операций: " + matrixChainOrder(dimensions));
         scanner.close();
+
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
+            int value = (int) (Math.random() * 100);
+            arr1[i] = value;
+        }
+
+        System.out.println("\n selectionSort:");
+        long startTime = System.nanoTime();
+        selectionSort(arr1);
+        long endTime = System.nanoTime();
+        System.out.println("Time taken to sort array: " + ((endTime - startTime)/1000000) + " ms");
+
+        System.out.println("\n mergeSort:");
+        fillingArray();
+        startTime = System.nanoTime();
+        mergeSort(arr1, 0,NUM_ELEMENTS-1);
+        endTime = System.nanoTime();
+        System.out.println("Time taken to sort array: " + ((endTime - startTime)/1000000) + " ms");
+
+        System.out.println("\n findMax:");
+        fillingArray();
+        startTime = System.nanoTime();
+        findMax(arr1);
+        endTime = System.nanoTime();
+        System.out.println("Time taken to sort array: " + ((endTime - startTime)/1000000) + " ms");
+
+
     }
 
     public static int coinChange(int[] coins, int amount) {
@@ -54,6 +87,13 @@ public class Main {
 
 
     }
+
+    private static void fillingArray() {
+        System.arraycopy(arr1, 0, arr, 0, n);
+    }
+
+
+    //Сложность O(n^2): Сортировка выбором (Selection Sort)
     public static void selectionSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
@@ -68,30 +108,58 @@ public class Main {
             arr[i] = temp;
         }
     }
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+
+    //Сложность O(n*log(n)): Сортировка слиянием (Merge Sort)
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    public static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return i + 1;
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
+
+    //Сложность O(n): Поиск максимального элемента в массиве
     public static int findMax(int[] arr) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
